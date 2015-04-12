4b74c6d09b4c51e9025cb41b94235d966110cf88
Aayush Kumar <iaayush.stevejobs@gmail.com>
2015-04-12 23:13:30 +0530

    Adding Complaint Object

---
+++ src/main/java/com/inventory_management_system/model/Complaint.java
@@ -0,0 +1,55 @@
+package com.inventory_management_system.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "complaints")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Complaint implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "subject")
+    private String subject;
+
+    @Column(name = "complaint_description")
+    private String complaintDescription;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getComplaintDescription() {
+        return complaintDescription;
+    }
+
+    public void setComplaintDescription(String complaintDescription) {
+        this.complaintDescription = complaintDescription;
+    }
+}
+
